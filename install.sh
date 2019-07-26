360dc366fc6dc5ec45bdade831606e3cd51d7c33
jnkng <jnkn6.dev@gmail.com>
2019-07-26 21:33:38 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+ROOTDIR=${PWD}
+MODULES=${ROOTDIR}/modules
+
+#portspoof
+echo "Install portspoof....."
+sudo apt-get install g++ make
+
+cd $MODULES/portspoof
+./configure
+make
+sudo make install
+sudo cp ./system_files/init.d/portspoof.sh /etc/init.d/portspoof
+sudo chmod +x /etc/init.d/portspoof
+
+#cowrie
+echo "Install cowrie......"
+sudo apt-get install git python-virtualenv libssl-dev libffi-dev build-essential libpython3-dev python3-minimal authbind
+#sudo adduser --disabled-password cowrie
+
+#sudo su - cowrie <<EOF
+cd $MODULES/cowrie
+virtualenv --python=python3 cowrie-env
+source cowrie-env/bin/activate
+pip install --upgrade pip
+pip install --upgrade -r requirements.txt
+pip install bcrypt
+
+
